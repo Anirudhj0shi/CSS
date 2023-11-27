7bd80976c237dc39d2f69412e67212c70f414bc1
Anirudh joshi <yashjoshi5521@gmail.com>
2023-11-27 16:58:52 +0530

    Day5_React

---
+++ Day5_React/Volcanus_React/src/App.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const App = () => {
+
+  const name = 'Ram';
+  return (
+    <>
+    <div>
+      <h1>my name is {name}</h1>
+      <p>your name is {45+68}</p>
+    </div>
+    <div>
+      <h1>hello motherfucker</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
