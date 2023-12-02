f7f0b7c3652fe4a3d31b50f84ec91d30e42ac5ce
Anirudh joshi <yashjoshi5521@gmail.com>
2023-12-02 16:54:11 +0530

    Day10_React_Api-Datafetch_Use-Effect

---
+++ Day10_React/Volcanus_React/src/App.jsx
@@ -0,0 +1,138 @@
+
+// {
+//   import React from 'react';
+// import Person from "./Components/Person"; 
+// import Aadharcard from "./Components/Aadharcard" 
+
+
+// const App = () => 
+// {
+
+//   return (
+//     <>
+//     <div className='superman'>
+//       <Person name={"laxman"} age={22} gmail={"laxman@gmail.com"}
+//       fathername={"ashok joshi"}/><br/>
+//       <Person name={"yash"} age={72} gmail={"YAsh@gmail.com"}/><br/>
+                
+//       <Person /><br/>
+//       <Aadharcard />
+//     </div>
+//     </>
+//   );
+
+// };
+
+// export default App }
+
+// .............
+
+
+// import React from 'react';
+// import Counter from "./Components/Counter";
+
+
+// const App = () => 
+// {
+  
+  //   return (
+    //     <>
+    //     <Counter />
+    
+//     </>
+//   )
+
+// };
+
+// export default App
+
+// .............
+
+
+// --------------------------------------------------------
+
+// import React from 'react';
+// import Userstateexam from "./Components/Userstateexam";
+
+
+// const App = () => 
+// {
+  
+//     return (
+//         <>
+//         <Userstateexam/>
+    
+//     </>
+//   )
+
+// };
+
+// export default App
+
+// -----------------------------------------------------------
+// import React from 'react';
+// import Products from './Components/Products'
+
+
+// const App = () => 
+// {
+  //    return (
+    //     <>
+    //     <div>
+    //         {/* <Products product={product} /> */}
+    //         <Products />
+    
+    //     </div>
+    //     </>
+    //   )
+
+// };
+
+// export default App;
+
+// -----------------------------------------------------------
+
+// import React from 'react';
+
+// import Use_Effect from './Components/Use_Effect';
+
+
+// const App = () => 
+// {
+//    return (
+//      <>
+//     <div>
+        
+//         <Use_Effect />
+
+//     </div>
+//     </>
+//   )
+
+// };
+
+// export default App;
+
+// -----------------------------------------------------------
+
+import React from 'react';
+import Fetch_data from './Components/Fetch_data';
+
+
+
+
+const App = () => 
+{
+   return (
+     <>
+    <div>
+        
+        <Fetch_data />
+
+    </div>
+    </>
+  )
+
+};
+
+export default App;
